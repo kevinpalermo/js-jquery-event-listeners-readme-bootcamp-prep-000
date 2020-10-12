062e478ebb29bdfcc89015f0e6fbf86120c255ef
Kevin Palermo <kevpalermo@gmail.com>
2020-10-12 22:42:31 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
 //define functions here
 
+function getIt(){
+  $("p").on("click", function() {
+  return alert("Hey!")
+});
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+  var minion = document.querySelector('img')
+    minion.classList.add("tasty")
+  })
+}
+
+function pressIt(){
+  $()
+}
+
 $(document).ready(function(){
-
-// call functions here
-
+ getIt();
+ frameIt();
 });
