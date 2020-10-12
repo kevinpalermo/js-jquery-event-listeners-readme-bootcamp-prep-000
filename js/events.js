a447798feba3591337814ec109f8f05a5dad78d6
Kevin Palermo <kevpalermo@gmail.com>
2020-10-12 22:46:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $("form").on("keydown", function(key){
+  $(document).on("keydown", function(key){
     if(key.which == 103){
       alert("G was pressed!");
     }
